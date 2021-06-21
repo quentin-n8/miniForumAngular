97529fdb892ce8a8c7fa4dd8895d7589a523b30c
Steffie <steffiehu23@gmail.com>
2021-06-21 15:25:17 +0200

    form modifier compte

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
+import { FormModifierCompteComponent } from './form-modifier-compte/form-modifier-compte.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainMenuComponent
+    MainMenuComponent,
+    FormModifierCompteComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/form-modifier-compte/form-modifier-compte.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-modifier-compte',
+  templateUrl: './form-modifier-compte.component.html',
+  styleUrls: ['./form-modifier-compte.component.css']
+})
+export class FormModifierCompteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
