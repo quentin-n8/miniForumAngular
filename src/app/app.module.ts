f23440bf6fd3731838b02495ae1e103071a479ab
qn <quentin.nguyen@gadz.org>
2021-06-22 11:59:46 +0200

    update_users

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { CreationFormComponent } from './creation-form/creation-form.component';
+import { UsersService } from './services/users.service';
 
 const routes: Routes = [
   { path: '', component: AppComponent },
+  { path: "createUserForm", component: CreationFormComponent },
   // { path: 'not-found', component: NotFoundComponent},    ###TODO create not-found page
   { path: '**', redirectTo: ''}                    //###TODO reasign to not-found page when created
 ]
@@ -14,14 +17,10 @@ const routes: Routes = [
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CreationFormComponent,
-  ],
-  imports: [
-    BrowserModule, ReactiveFormsModule
-  ],
-  providers: [],
+  declarations: [AppComponent, CreationFormComponent],
+  imports: [HttpClientModule, BrowserModule, ReactiveFormsModule, RouterModule.forRoot(routes)],
+  providers: [UsersService],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
--- src/app/creation-form/creation-form.component.ts
+++ src/app/creation-form/creation-form.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { UsersService } from '../services/users.service';
+import { User } from "../modeles/User";
 
 @Component({
   selector: 'app-creation-form',
   templateUrl: './creation-form.component.html',
   styleUrls: ['./creation-form.component.css']
 })
+
 export class CreationFormComponent implements OnInit {
   CreationForm!: FormGroup;
+  user!: User;
   
-
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private userservice: UsersService, private formBuilder: FormBuilder) {
+   }
 
   ngOnInit(): void {
     this.CreationForm= this.formBuilder.group({
@@ -22,6 +26,9 @@ export class CreationFormComponent implements OnInit {
 
   onSubmit(): void {
     console.log(this.CreationForm.value);
+    this.user= this.CreationForm.value;
+    this.userservice.createUser(this.user);
+
   }
 
 }
