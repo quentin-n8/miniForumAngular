48a266b8644d179d3392a562feac367bfc944203
Steffie <steffiehu23@gmail.com>
2021-06-24 11:09:33 +0200

    Changements mineurs

--- src/app/form-modifier-compte/form-modifier-compte.component.ts
+++ src/app/form-modifier-compte/form-modifier-compte.component.ts
@@ -5,7 +5,8 @@ import { AbstractControl } from '@angular/forms';
 import { ValidationErrors } from '@angular/forms';
 import { User } from '../modeles/User';
 import { UsersService } from '../services/users.service';
-import {FormControl} from '@angular/forms';
+import { FormControl } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-form-modifier-compte',
@@ -17,12 +18,13 @@ export class FormModifierCompteComponent implements OnInit {
 
   myForm!: FormGroup;
   user!: User;
-  listeUsers: User[]=[];
+  listeUsers: User[] = [];
+
 
 
   constructor(private userService: UsersService, private formBuilder: FormBuilder) {
-  
-}
+
+  }
 
   ngOnInit(): void {
     this.myForm = this.formBuilder.group({
@@ -30,33 +32,20 @@ export class FormModifierCompteComponent implements OnInit {
       nvPassword: ['', [Validators.minLength(3), Validators.maxLength(50), Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{3,50}$/)]],
       confirPassword: ['', [Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{3,50}$/), this.passwordConfirmValidator()]],
       actuelPassword: ['', [Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{3,50}$/)]],
-   
-    })
 
+    })
+    
   }
 
   onSubmit(): void {
-    
-    console.log(this.myForm.value);
-    console.log(this.user);
-  
-     let user={
-       username:this.myForm.value.nvUsername,
-       password:this.myForm.value.nvPassword,
-       passwordConfirm: this.myForm.value.confirPassword,
-       oldPassword: this.myForm.value.actuelPassword
-      };
-    // console.log(this.myForm.value.nvUsername);
 
-    // this.user.username=this.myForm.value.nvUsername;
-    // console.log(this.user.username);
-    
-    // this.user.password = this.myForm.value.nvPassword;
-    // console.log(this.user.password);
-    
-    // console.log(this.user);
+    let user = {
+      username: this.myForm.value.nvUsername,
+      password: this.myForm.value.nvPassword,
+      passwordConfirm: this.myForm.value.confirPassword,
+      oldPassword: this.myForm.value.actuelPassword
+    };
     this.userService.modifierUnUser(3, user)
-    
   }
 
   getUsernameErrors(): string | void {
@@ -69,7 +58,7 @@ export class FormModifierCompteComponent implements OnInit {
     }
   }
 
-  getActuelPasswordErrors(): string|void{
+  getActuelPasswordErrors(): string | void {
     if (this.myForm.controls.actuelPassword.hasError('pattern')) {
       return "Le mot de passe doit contenir au moins: un chiffre, une majuscule et un caractère spécial"
     }
@@ -97,7 +86,24 @@ export class FormModifierCompteComponent implements OnInit {
   passwordConfirmValidator(): ValidatorFn {
 
     return (control: AbstractControl): {
-      [key: string]: any} | null => {
+      [key: string]: any
+    } | null => {
+      if (this.myForm && (this.myForm.value.nvPassword !== control.value)) {
+        return {
+          passwordConfirm: { value: '' }
+        };
+      }
+      else {
+        return null;
+      }
+    }
+  };
+
+  UsernameConfirmValidator(): ValidatorFn {
+
+    return (control: AbstractControl): {
+      [key: string]: any
+    } | null => {
       if (this.myForm && (this.myForm.value.nvPassword !== control.value)) {
         return {
           passwordConfirm: { value: '' }
